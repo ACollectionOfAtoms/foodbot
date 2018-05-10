55a617a18a5f3a578950c6105cd256a314d28254
Adam <davidatomhernandez@gmail.com>
2018-05-10 18:25:09 -0400

    use new events api

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
 )
 
 var (
@@ -15,7 +19,7 @@ var (
 	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
 )
 
-// Handle Slack challenge request.
+// ChallengeHandler Handle Slack challenge request.
 func ChallengeHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	body := request.Body
 	type ChallengeResponse struct {
@@ -44,6 +48,7 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// stdout and stderr go to Cloudwatch!
 	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
 
+	// TODO: check if type = url_verification
 	if strings.Contains(request.Body, "challenge") {
 		return ChallengeHandler(request)
 	}
@@ -52,11 +57,39 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		return events.APIGatewayProxyResponse{}, ErrNameNotProvided
 	}
 
+	// Use Slack events API to process response
+	slackToken := os.Getenv("SLACK_API_KEY")
+	api := slack.New(slackToken)
+	params := slack.PostMessageParameters{}
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(request.Body))
+	if e != nil {
+		log.Printf("Error!, %s", e)
+	}
+	innerEvent := eventsAPIEvent.InnerEvent
+	switch ev := innerEvent.Data.(type) {
+	// This is an EventsAPI specific event
+	case *slackevents.AppMentionEvent:
+		api.PostMessage(ev.Channel, "hi", params)
+		break
+	// There are Events API specific MessageEvents
+	// https://api.slack.com/events/message.channels
+	case *slackevents.MessageEvent:
+		if strings.Contains(ev.Text, "pizza") {
+			api.PostMessage(ev.Channel, "I like pizza too.", params)
+		}
+		break
+	// This is an Event shared between RTM and the EventsAPI
+	case *slack.ChannelCreatedEvent:
+		api.PostMessage(ev.Channel.ID, "Oh hey, a new channel!", params)
+		break
+	default:
+		fmt.Println(innerEvent.Type)
+		fmt.Println("no event to match")
+	}
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello " + request.Body,
+		Body:       "Event:" + string(innerEvent.Type) + ", recieved!",
 		StatusCode: 200,
 	}, nil
-
 }
 
 func main() {
